0c85c36807093cdaa715d4d55e1b9873ac3a67fe
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-02-12 13:20:35 -0500

    happy birthday wayming wing

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import FroyoCreate from '@/views/liu.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'froyo',
+      component: FroyoCreate,
+    },
+  ],
 })
 
 export default router
